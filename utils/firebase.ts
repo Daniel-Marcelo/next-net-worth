36ced8fded26a701b4c26c92535ddaa92770422c
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2024-02-07 22:46:37 +0000

    Add firebase config

---
+++ utils/firebase.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const f = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env,
+};
+console.log(f);
+
+export const firebase = initializeApp({
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+});
+export const db = getFirestore(firebase);
+
+export const auth = getAuth(firebase);
