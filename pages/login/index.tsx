f3a6db21f1ddaba555bbac95ed8e9d07e4b1a08a
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2024-02-11 20:07:48 +0000

    Create app bar

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,10 +2,16 @@ import yahooFinance from "yahoo-finance2";
 import { GoogleLoginButton } from "react-social-login-buttons";
 import { useLoginWithGoogle } from "../../hooks/useAuth";
 import { withPublic } from "../../components/RouteProtection";
+import { DrawerAppBar } from "../../components/NavBarV2";
 
 function Page({ data }) {
   const loginWithGoogleMutation = useLoginWithGoogle();
-  return <GoogleLoginButton onClick={loginWithGoogleMutation.mutate} />;
+  return (
+    <>
+      <DrawerAppBar />
+      <GoogleLoginButton onClick={loginWithGoogleMutation.mutate} />
+    </>
+  );
 }
 
 export async function getServerSideProps(props) {
