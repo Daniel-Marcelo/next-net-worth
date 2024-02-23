fa21a01b8bab56c8305f41cc16e14040cf38a75b
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2024-02-23 18:40:40 +0000

    Create ticker search api endpoint

--- pages/api/ticker-search/[id].ts
+++ pages/api/ticker-search/[id].ts
@@ -1,8 +1,52 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import yahooFinance from "yahoo-finance2";
+import {
+  SearchQuoteNonYahoo,
+  SearchQuoteYahooCryptocurrency,
+  SearchQuoteYahooCurrency,
+  SearchQuoteYahooETF,
+  SearchQuoteYahooEquity,
+  SearchQuoteYahooFund,
+  SearchQuoteYahooFuture,
+  SearchQuoteYahooIndex,
+  SearchQuoteYahooOption,
+} from "yahoo-finance2/dist/esm/src/modules/search";
+import {
+  specialCryptoSites,
+  specialIndexFundSites,
+} from "../../../const/specialSites.constants";
 
 type ResponseData = {
   message: string;
+};
+
+type QuoteResult =
+  | SearchQuoteYahooEquity
+  | SearchQuoteYahooOption
+  | SearchQuoteYahooETF
+  | SearchQuoteYahooFund
+  | SearchQuoteYahooIndex
+  | SearchQuoteYahooCurrency
+  | SearchQuoteYahooCryptocurrency
+  | SearchQuoteNonYahoo
+  | SearchQuoteYahooFuture;
+
+const AddSpecialSite = (quote: QuoteResult) => {
+  if (quote.longname) {
+    for (const site of specialIndexFundSites) {
+      if (quote.longname.toLowerCase().includes(site.text.toLowerCase())) {
+        return site.site;
+      }
+    }
+    for (const site of specialCryptoSites) {
+      if (
+        quote.longname.toLowerCase().includes(site.text.toLowerCase()) ||
+        quote.longname.toLowerCase().includes(site.ticker.toLowerCase())
+      ) {
+        return site.site;
+      }
+    }
+  }
 };
 
 export default async function handler(
@@ -11,6 +55,34 @@ export default async function handler(
 ) {
   if (req.method === "GET" && req.query.id) {
     const response = await yahooFinance.search(req.query.id as string);
+    const getWebsites = async () => {
+      for await (const num of response.quotes) {
+        if (num.symbol) {
+          console.log("getting data for " + num.symbol);
+          try {
+            const innerResponse = await yahooFinance.quoteSummary(num.symbol, {
+              modules: ["summaryProfile"],
+            });
+
+            let website = innerResponse.summaryProfile.website;
+            console.log("checklis");
+            if (website) {
+              num.website = innerResponse.summaryProfile.website
+                .replaceAll("https://", "")
+                .replaceAll("http://", "")
+                .replaceAll("www.", "");
+            }
+          } catch {
+            console.log("error getting data for " + num.symbol);
+            continue;
+          }
+          if (!num.website) {
+            num.website = AddSpecialSite(num);
+          }
+        }
+      }
+    };
+    await getWebsites();
     res.status(200).json(JSON.parse(JSON.stringify(response)));
   } else {
     res.status(405);
