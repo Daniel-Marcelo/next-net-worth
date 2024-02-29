c1d9bd6a82b6c11d12c5b9d66a4a77e91cbbcf53
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2024-02-29 03:47:44 +0000

    Update signout logic git push

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -1,12 +1,9 @@
 import { Box, Button, Typography } from "@mui/material";
 import axios from "axios";
 import { useMutation } from "@tanstack/react-query";
+import { signOut } from "next-auth/react";
 
 export function Page() {
-  const mutation = useMutation({
-    mutationKey: ["logout"],
-    mutationFn: () => axios.post("api/logout"),
-  });
   return (
     <Box sx={{ p: "2rem" }}>
       <Typography variant="h3" sx={{ textAlign: "center" }}>
@@ -14,7 +11,7 @@ export function Page() {
       </Typography>
       <Button
         variant="contained"
-        onClick={() => mutation.mutate()}
+        onClick={() => signOut()}
         fullWidth
         sx={{ mt: "2rem" }}
       >
