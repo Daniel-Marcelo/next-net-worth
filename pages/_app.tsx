c256f4697371536b2fbead3aa7faabf3a3d312ec
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2024-06-30 22:14:26 +0100

    Add support for messaging

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ import "@fontsource/roboto/700.css";
 import "../styles/global.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SessionProvider } from "next-auth/react";
 
 import { FixedBottomNavigation } from "../components/BottomNav/BottomNav";
@@ -25,6 +25,24 @@ export default function MyApp({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  const [a, setA] = useState<any>(new Set());
+  useEffect(() => {
+    const messageListener = window.addEventListener(
+      "message",
+      (nativeEvent) => {
+        console.log(nativeEvent?.data);
+        if (nativeEvent.source) {
+          setA(new Set([...a, nativeEvent.data.source]));
+        }
+      }
+    );
+    return messageListener;
+  }, []);
+
+  // method to send msg to react native
+  const sendMessage = () => {
+    (window as any)?.ReactNativeWebView.postMessage("Hi from PWA");
+  };
   // Instead do this, which ensures each request has its own cache:
   const [queryClient] = useState(
     () =>
@@ -41,6 +59,10 @@ export default function MyApp({
 
   return (
     <SessionProvider session={session}>
+      <button onClick={sendMessage}>{` Say Hi`}</button>
+      {Array.from(a).map((source) => (
+        <div>{source as any}</div>
+      ))}
       <QueryClientProvider client={queryClient}>
         <HydrationBoundary state={pageProps.dehydratedState}>
           <ThemeProvider theme={darkTheme}>
