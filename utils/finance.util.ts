38f083ff09c3922b6e6395e66adb6771375fc273
DanielMcKeeAlpha <daniel.mckee@alphasights.com>
2024-07-03 21:34:57 +0100

    Add missing modules method

--- utils/finance.util.ts
+++ utils/finance.util.ts
@@ -1,7 +1,14 @@
 import yahooFinance from "yahoo-finance2";
+import { QuoteSummaryModules } from "yahoo-finance2/dist/esm/src/modules/quoteSummary";
 import { QuoteSummaryResult } from "yahoo-finance2/dist/esm/src/modules/quoteSummary-iface";
 
 export const financeApi = {
+  getModules: async (symbol: string, ...modules: QuoteSummaryModules[]) => {
+    const response = await yahooFinance.quoteSummary(symbol, {
+      modules,
+    });
+    return response;
+  },
   getSummaryProfile: async (symbol: string) => {
     const response = await yahooFinance.quoteSummary(symbol, {
       modules: ["summaryProfile"],
